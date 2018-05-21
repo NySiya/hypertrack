eb8fb8922fc2166d28f787207de5b7d366d22522
Siya Ny <siya.nyinfo@gmail.com>
2018-05-21 23:07:07 +0700

    fixed missing m2repository

--- App.js
+++ App.js
@@ -7,35 +7,46 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { NativeModules } from 'react-native';
-import {RNHyperTrack as RNHyperTrackImport} from 'react-native-hypertrack';
+import { RNHyperTrack as RNHyperTrackImport } from 'react-native-hypertrack';
 
 if (NativeModules.RNHyperTrack != undefined) {
   var RNHyperTrack = NativeModules.RNHyperTrack;
 } else {
   var RNHyperTrack = RNHyperTrackImport;
-}
+};
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     RNHyperTrack.initialize('pk_test_f898ad1a09558cd87a86c0ee5bf5df68f0ee8287');
+    RNHyperTrack.requestAlwaysLocationAuthorization('Location', 'Asking for Location Permission');
+    // RNHyperTrack.requestAlwaysLocationAuthorization(title, message);
+    if (Platform.os === 'ios') {
+      RNHyperTrack.requestMotionAuthorization();
+    };
   }
 
-  createUer() {
-    HyperTrack.getOrCreateUser(name: name, phone: phone, uniqueId: phone) { (user, error) in
-      if (error != nil) {
-        return error
-      }
+  localAuthorization() {
+    RNHyperTrack.locationAuthorizationStatus().then((status) => {
+      // Handle locationAuthorizationStatus API promise here
+      console.log('locationAuthorizationStatus: ', status);
+    });
+  }
 
-      if (user != nil) {
-        return 'success'
+  checkLocationService() {
+    RNHyperTrack.locationServicesEnabled().then(
+      (callback) => {
+        // Handle locationServicesEnabled API callback here
+        console.log('locationServicesEnabled: ', callback);
       }
-    }
+    );
   }
 
   render() {
+    this.checkLocationService();
+    this.localAuthorization();
     return (
-      <Text>{ this.createUer }<Text>
+      <Text>hihi</Text>
     );
   }
 }
